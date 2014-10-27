1c4c59eddef0988f7d7fd90f9fd24222b557060d
Hai Kieu <haikieu2907@gmail.com>
2014-10-28 00:51:54 +0700

    Migrate to HKDataRequestCenter file

--- Classes/DataRequestCenter/HKDataRequestCenterDelegate.h
+++ Classes/DataRequestCenter/HKDataRequestCenterDelegate.h
@@ -6,13 +6,3 @@
 //  Copyright (c) 2014 haikieu2907@gmail.com. All rights reserved.
 //
 
-@protocol HKDataRequestCenterDelegate <NSObject>
-
--(void)networkActivityStarted;
--(void)networkActivityEnded;
-
--(void)networkActivityBlockingStarted;
--(void)networkActivityBlockingEnded;
--(void)networkActivityBlockingChanged:(NSString*)status;
-
-@end
